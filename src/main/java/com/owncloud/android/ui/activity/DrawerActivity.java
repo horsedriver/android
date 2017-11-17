8c56cdb6ec60bd8c7f375848aaaf1908e327eac0
tobiaskaminsky <tobias@kaminsky.me>
2017-11-17 13:39:35 +0100

    prevent crash when updating external links

--- src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
@@ -1354,7 +1354,7 @@ public abstract class DrawerActivity extends ToolbarActivity implements DisplayU
                                     externalLinksProvider.storeExternalLink(link);
                                 }
 
-                                updateExternalLinksInDrawer();
+                                runOnUiThread(() -> updateExternalLinksInDrawer());
                             }
                         } else {
                             sharedPreferences.edit().putInt(EXTERNAL_LINKS_COUNT, count + 1).apply();
